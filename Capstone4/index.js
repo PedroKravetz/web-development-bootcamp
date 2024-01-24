cd5302911e58230570fdfc29b698c91e85eb4f9f
PedroKravetz <p.v.kravetz@gmail.com>
2024-01-24 15:38:48 -0300

    Concluído Public API de cocktails

--- Capstone4/index.js
+++ Capstone4/index.js
@@ -16,13 +16,71 @@ app.get("/", (req, res) => {
 
 app.get("/random", async (req, res) => {
   const result = await axios.get(API_URL + "random.php");
-  console.log(result.data);
-  res.render("index.ejs");
+  var ingredients = [];
+  for (var i = 1; i < 16; i++) {
+    if (result.data.drinks[0]["strMeasure" + i]) {
+      ingredients.push(
+        result.data.drinks[0]["strMeasure" + i] +
+          " of " +
+          result.data.drinks[0]["strIngredient" + i]
+      );
+    } else {
+      break;
+    }
+  }
+  res.render("index.ejs", {
+    name: result.data.drinks[0]["strDrink"],
+    instructions: result.data.drinks[0]["strInstructions"],
+    ingredients: ingredients,
+    image: result.data.drinks[0]["strDrinkThumb"],
+  });
 });
 
 app.post("/search", async (req, res) => {
-  console.log(req.body);
-  res.render("index.ejs");
+  if (req.body.search !== "") {
+    var body = [];
+    if (req.body.filter === "Ingredient") {
+      const result = await axios.get(
+        API_URL + "search.php?i=" + req.body.search
+      );
+      for (var ingredient of result.data.ingredients) {
+        var name = ingredient["strIngredient"].toLowerCase();
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+        body.push({
+          name: ingredient["strIngredient"],
+          description: ingredient["strDescription"],
+          image:
+            "https://www.thecocktaildb.com/images/ingredients/" + name + ".png",
+        });
+      }
+      res.render("index.ejs", { ingredients: body });
+    } else {
+      const result = await axios.get(
+        API_URL + "search.php?s=" + req.body.search
+      );
+      for (var drink of result.data.drinks) {
+        var ingredients = [];
+        for (var i = 1; i < 16; i++) {
+          if (drink["strMeasure" + i]) {
+            ingredients.push(
+              drink["strMeasure" + i] + " of " + drink["strIngredient" + i]
+            );
+          } else {
+            break;
+          }
+        }
+        body.push({
+          name: drink["strDrink"],
+          instructions: drink["strInstructions"],
+          image: drink["strDrinkThumb"],
+          ingredients: ingredients,
+        });
+      }
+      res.render("index.ejs", { drinks: body });
+    }
+  } else {
+    res.render("index.ejs");
+  }
 });
 
 app.listen(port, () => {
--- Capstone4/public/header.js
+++ Capstone4/public/header.js
@@ -1,18 +1,9 @@
 $("#filter").on("click", function (event) {
   if ($("#filter").text() === "Ingredient") {
     $("#filter").text("Drink");
+    $("#Filter").val("Drink");
   } else {
     $("#filter").text("Ingredient");
-  }
-});
-
-$("#searchBT").on("click", function (event) {
-  console.log("search");
-  if ($("#search").val().trim() !== "") {
-    console.log("search2");
-    $.post("/search", {
-      filter: $("#filter").text().trim(),
-      search: $("#search").val().trim()
-    });
+    $("#Filter").val("Ingredient");
   }
 });
