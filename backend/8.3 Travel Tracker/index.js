6ff6988e74a136eed3cc8a1f2d48abfcfbac7f6d
PedroKravetz <p.v.kravetz@gmail.com>
2024-01-26 14:26:12 -0300

    Travel tracker finished

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -26,8 +26,40 @@ app.get("/", async (req, res) => {
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-
   res.render("index.ejs", { total: countries.length, countries: countries });
+});
+
+app.post("/add", async (req, res) => {
+  if (req.body.country && req.body.country.trim() != "") {
+    const country = req.body.country.trim().toLowerCase();
+    const result = await db.query(
+      "select country_code from countries where LOWER(country_name) like '%' || $1 || '%';",
+      [country]
+    );
+    if (result.rows.length > 0) {
+      try {
+        const insert = await db.query(
+          "insert into visited_countries (country_code) values($1)",
+          [result.rows[0].country_code]
+        );
+        res.redirect("/");
+      } catch {
+        res.render("index.ejs", {
+          total: countries.length,
+          countries: countries,
+          error: "Country has already been added, try again.",
+        });
+      }
+    } else {
+      res.render("index.ejs", {
+        total: countries.length,
+        countries: countries,
+        error: "Country does not exist, try again.",
+      });
+    }
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
